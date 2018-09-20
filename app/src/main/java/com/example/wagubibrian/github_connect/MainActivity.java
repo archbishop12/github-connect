9279c0f9fcfc912aa5895c80abf5aa3bfdf84e10
Wagubi Brian <brian.wagubi@andela.com>
2018-09-20 10:57:34 +0300

    feat(layout): Connect recyclerview to mainactivity - Add recyclerview reference to mainactivity class [Delivers #155768857]

--- app/src/main/java/com/example/wagubibrian/github_connect/MainActivity.java
+++ app/src/main/java/com/example/wagubibrian/github_connect/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.wagubibrian.github_connect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.github_list);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.github_list);
+        mRecyclerView.setHasFixedSize(true);
+
     }
 }
