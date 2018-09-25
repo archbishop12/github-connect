535717ec24e357df1a39a8e2f671c5868a19b39e
Wagubi Brian <brian.wagubi@andela.com>
2018-09-25 13:56:57 +0300

    feat(view): Handling configurations changes  - Make GihubUsers class parcelable  - Add code in mainactivty for handling configurations changes [Delivers #156005187]

--- app/src/main/java/com/example/wagubibrian/github_connect/view/MainActivity.java
+++ app/src/main/java/com/example/wagubibrian/github_connect/view/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.wagubibrian.github_connect.view;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,9 @@ import com.example.wagubibrian.github_connect.R;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public final static String LIST_STATE_KEY = "recycler_state_list";
+    Parcelable listState;
     private Button testButton;
 
     @Override
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView.setHasFixedSize(true);
 
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +41,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        listState = mLayoutManager.onSaveInstanceState();
+        outState.putParcelable(LIST_STATE_KEY, listState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            listState = savedInstanceState.getParcelable(LIST_STATE_KEY);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (listState != null) {
+            mLayoutManager.onRestoreInstanceState(listState);
+        }
     }
 }
