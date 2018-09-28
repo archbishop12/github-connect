9ff77ce1e2efb79d48d644fa6277b8b2474659eb
Wagubi Brian <brian.wagubi@andela.com>
2018-09-28 14:34:01 +0300

    feat(share):share functionality - add share intent in the detail activity [Delivers #156008359]

--- app/src/main/java/com/example/wagubibrian/github_connect/view/DetailActivity.java
+++ app/src/main/java/com/example/wagubibrian/github_connect/view/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.wagubibrian.github_connect.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ public class DetailActivity extends AppCompatActivity {
     private ImageView profileImage;
     private ImageView gitIcon;
     private ImageView linkIcon;
+    private ImageView shareImage;
     private TextView userName;
     private TextView htmlURL;
 
@@ -26,10 +29,11 @@ public class DetailActivity extends AppCompatActivity {
         profileImage = (ImageView)findViewById(R.id.profileDetail);
         gitIcon = (ImageView)findViewById(R.id.github_icon);
         linkIcon = (ImageView)findViewById(R.id.link_image);
+        shareImage = (ImageView)findViewById(R.id.share_button);
         userName = (TextView)findViewById(R.id.user_name);
         htmlURL = (TextView)findViewById(R.id.url_text);
 
-        GithubUsers gitUser = getIntent().getExtras().getParcelable("User");
+        final GithubUsers gitUser = getIntent().getExtras().getParcelable("User");
 
         Glide.with(getApplicationContext())
                 .load(gitUser.getProfileImage())
@@ -37,8 +41,19 @@ public class DetailActivity extends AppCompatActivity {
                 .into(profileImage);
         gitIcon.setImageResource(R.drawable.ic_icons8_github);
         linkIcon.setImageResource(R.drawable.ic_link_black_24dp);
+        shareImage.setImageResource(R.drawable.ic_share_black_24dp);
         userName.setText(gitUser.getUserName());
         htmlURL.setText(gitUser.getHtmlUrl());
+
+        shareImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Check out this awesome developer @"+gitUser.getUserName()+", "+gitUser.getHtmlUrl()+".");
+                startActivity(Intent.createChooser(shareIntent, "Share link using"));
+            }
+        });
 
     }
 }
