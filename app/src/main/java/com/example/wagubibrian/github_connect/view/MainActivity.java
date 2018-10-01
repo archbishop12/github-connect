b612b42bad0243a9252a432f6b6e976a865ff567
Wagubi Brian <brian.wagubi@andela.com>
2018-10-01 07:19:34 +0300

    feat(view): ui/ux revamp - switched listview to gridview layout - implemented swipetorefresh layout - added progress dialog [Delivers #156037248]

--- app/src/main/java/com/example/wagubibrian/github_connect/view/MainActivity.java
+++ app/src/main/java/com/example/wagubibrian/github_connect/view/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.wagubibrian.github_connect.view;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Parcelable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -17,15 +21,31 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     public final static String LIST_STATE_KEY = "recycler_state_list";
-    Parcelable listState;
+    private Parcelable listState;
+    private GithubPresenter githubPresenter;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.github_list);
+
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_layout);
+        swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Refresh items
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Loading...");
+                progressDialog.show();
+                refreshItems();
+            }
+        });
 
         mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -33,9 +53,15 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        GithubPresenter githubPresenter = new GithubPresenter();
+        githubPresenter = new GithubPresenter();
         githubPresenter.getGithubUsers(MainActivity.this);
 
+    }
+
+    private void refreshItems() {
+        githubPresenter.getGithubUsers(MainActivity.this);
+        swipeRefreshLayout.setRefreshing(false);
+        progressDialog.dismiss();
     }
 
     @Override
@@ -63,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void populateRecyclerView(List<GithubUsers> githubUsers){
 
-        mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        mLayoutManager = new GridLayoutManager(getApplicationContext(), 1);
 
         GithubUsersAdapter githubUsersAdapter = new GithubUsersAdapter(getApplicationContext(), (ArrayList<GithubUsers>) githubUsers);
         mRecyclerView.setAdapter(githubUsersAdapter);
